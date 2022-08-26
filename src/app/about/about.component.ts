0ce8c5d3eea90b35d28a71c4882f4a1c75d62d19
ServandoLuviano <servando.luviano@aa.com>
2022-08-26 09:51:42 -0500

    asdfs

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.css'],
 })
 export class AboutComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    const source1$ = of(1, 2, 3);
+    const source2$ = of(4, 5, 6);
   }
-
 }
---
+++ src/app/utils/functions.ts
@@ -0,0 +1,23 @@
+import { Observable } from 'rxjs';
+
+export function createHttpObservable(url: string): Observable<any> {
+  return new Observable(observer => {
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    fetch(url, {
+      signal,
+    })
+      .then(response => {
+        return response.json();
+      })
+      .then(body => {
+        observer.next(body);
+        observer.complete();
+      })
+      .catch(error => {
+        observer.error(error);
+      });
+    return () => controller.abort();
+  });
+}
